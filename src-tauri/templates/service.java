aacaa4abeb794e08addfad6b02261abb2fe4bf44
FengYouJun <845225187@qq.com>
2023-02-02 18:37:09 +0800

    feat: 添加service数据渲染

--- src-tauri/templates/service.java
+++ src-tauri/templates/service.java
@@ -1,20 +1,20 @@
-package ${package.Service};
+package {{ package.Service }};
 
-import ${package.Entity}.${entity};
-import ${superServiceClassPackage};
+import {{ package.Entity }}.{{ entity }};
+import {{ superServiceClassPackage }};
 
 /**
  * <p>
- * ${table.comment!} 服务类
+ * {{ table.comment }} 服务类
  * </p>
  *
- * @author ${author}
- * @since ${date}
+ * @author {{ author }}
+ * @since {{ date }}
  */
-<#if kotlin>
-interface ${table.serviceName} : ${superServiceClass}<${entity}>
-<#else>
-public interface ${table.serviceName} extends ${superServiceClass}<${entity}> {
+{% if kotlin -%}
+interface {{ table.serviceName }} : {{ superServiceClass }}<{{ entity }}>
+{% else -%}
+public interface {{ table.serviceName }} extends {{ superServiceClass }}<{{ entity }}> {
 
 }
-</#if>
+{% endif %}
