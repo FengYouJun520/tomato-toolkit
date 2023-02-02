0b766ad14690aa6e6c935f889a69c5776280f5e1
FengYouJun <845225187@qq.com>
2023-02-02 18:40:29 +0800

    feat: 添加ServiceImpl数据渲染

--- src-tauri/templates/serviceImpl.java
+++ src-tauri/templates/serviceImpl.java
@@ -1,26 +1,26 @@
-package ${package.ServiceImpl};
+package {{ package.ServiceImpl }};
 
-import ${package.Entity}.${entity};
-import ${package.Mapper}.${table.mapperName};
-import ${package.Service}.${table.serviceName};
-import ${superServiceImplClassPackage};
+import {{ package.Entity }}.{{ entity }};
+import {{ package.Mapper }}.{{ table.mapperName }};
+import {{ package.Service }}.{{ table.serviceName }};
+import {{ superServiceImplClassPackage }};
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- * ${table.comment!} 服务实现类
+ * {{ table.comment }} 服务实现类
  * </p>
  *
- * @author ${author}
- * @since ${date}
+ * @author {{ author }}
+ * @since {{ date }}
  */
 @Service
-<#if kotlin>
-open class ${table.serviceImplName} : ${superServiceImplClass}<${table.mapperName}, ${entity}>(), ${table.serviceName} {
+{% if kotlin -%}
+open class {{ table.serviceImplName }} : {{ superServiceImplClass }}<{{ table.mapperName }}, {{ entity }}>(), {{ table.serviceName }} {
 
 }
-<#else>
-public class ${table.serviceImplName} extends ${superServiceImplClass}<${table.mapperName}, ${entity}> implements ${table.serviceName} {
+{% else -%}
+public class {{ table.serviceImplName }} extends {{ superServiceImplClass }}<{{ table.mapperName }}, {{ entity }}> implements {{ table.serviceName }} {
 
 }
-</#if>
+{% endif %}
